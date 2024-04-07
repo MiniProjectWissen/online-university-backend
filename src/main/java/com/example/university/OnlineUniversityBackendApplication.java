610627616b7fba3da4df8cfdd7cbd29cacf2f1ba
Shubham Shelar <85821105+shubhamshelar@users.noreply.github.com>
2024-04-07 18:08:33 +0530

    Initial commit

---
+++ src/main/java/com/example/university/OnlineUniversityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineUniversityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineUniversityBackendApplication.class, args);
+	}
+
+}
