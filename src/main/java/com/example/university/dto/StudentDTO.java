d16657939b2105c76e51bc51ce88f62156553642
Shubham Shelar <85821105+shubhamshelar@users.noreply.github.com>
2024-04-08 11:07:18 +0530

    added lombok

--- src/main/java/com/example/university/dto/StudentDTO.java
+++ src/main/java/com/example/university/dto/StudentDTO.java
@@ -4,6 +4,7 @@ public class StudentDTO {
 
 	public StudentDTO() {
 		
+		
 	}
 
 }
--- src/main/java/com/example/university/entity/Course.java
+++ src/main/java/com/example/university/entity/Course.java
@@ -18,7 +18,18 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Data
 public class Course {
 
 	@Id
@@ -45,114 +56,6 @@ public class Course {
 	@JoinColumn(name = "teacher_id")
 	private Teacher teacher;
 	
-	public Teacher getTeacher() {
-		return teacher;
-	}
-
-	public void setTeacher(Teacher teacher) {
-		this.teacher = teacher;
-	}
-
-	public int getCourse_id() {
-		return course_id;
-	}
-
-	public void setCourse_id(int course_id) {
-		this.course_id = course_id;
-	}
-
 	
-
-	public int getForum_id() {
-		return forum_id;
-	}
-
-	public void setForum_id(int forum_id) {
-		this.forum_id = forum_id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Date getStart_date() {
-		return start_date;
-	}
-
-	public void setStart_date(Date start_date) {
-		this.start_date = start_date;
-	}
-
-	public Date getEnd_date() {
-		return end_date;
-	}
-
-	public void setEnd_date(Date end_date) {
-		this.end_date = end_date;
-	}
-
-	public String getSch_days() {
-		return sch_days;
-	}
-
-	public void setSch_days(String sch_days) {
-		this.sch_days = sch_days;
-	}
-
-	public Blob getSyllabus() {
-		return syllabus;
-	}
-
-	public void setSyllabus(Blob syllabus) {
-		this.syllabus = syllabus;
-	}
-
-	public Time getJoin_time() {
-		return join_time;
-	}
-
-	public void setJoin_time(Time join_time) {
-		this.join_time = join_time;
-	}
-
-	public Time getEnd_time() {
-		return end_time;
-	}
-
-	public void setEnd_time(Time end_time) {
-		this.end_time = end_time;
-	}
-
-	public int getLectures_taken() {
-		return lectures_taken;
-	}
-
-	public void setLectures_taken(int lectures_taken) {
-		this.lectures_taken = lectures_taken;
-	}
-
-	public ArrayList<Student> getStudents_list() {
-		return students;
-	}
-
-	public void setStudents_list(ArrayList<Student> students) {
-		this.students = students;
-	}
-
-	public Course() {
-		
-	}
 
 }
